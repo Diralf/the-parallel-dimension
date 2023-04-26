75626200d21aad63032b7a30fc27e117cc289c1b
diralf <diralfeastarea@gmail.com>
2023-04-26 14:41:58 +0300

    setup phaser project with default scene

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-
+import phaserGame from './phaser/PhaserGame';
+import HelloWorldScene from './phaser/scenes/HelloWorldScene';
 
 window.onload = () => {
+    const scene = phaserGame.scene.keys.helloworld as HelloWorldScene;
+    // scene.createEmitter()
 };
